68b886ad109d2a4daa770660e3048db58241f25d
Mark Hall <mhall48@uwo.ca>
2015-03-05 19:44:29 -0500

    first commit

---
+++ SimpleTwitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTwitter
+//
+//  Created by Mark Hall on 2015-03-05.
+//  Copyright (c) 2015 Mark Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleTwitter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleTwitter
+//
+//  Created by Mark Hall on 2015-03-05.
+//  Copyright (c) 2015 Mark Hall. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
